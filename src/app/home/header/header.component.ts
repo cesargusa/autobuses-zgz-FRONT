6d2de57a00ac240fe4ab26627c6817fd66417db9
cesargusa <cesargusa22@gmail.com>
2023-04-10 21:44:26 +0200

    Añadido el Home component y el Header component

---
+++ src/app/home/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit,HostListener   } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  //Funcion para desplegar el submenu
+  changeMenuProfile() {
+    let menuProfile = document.getElementById('dropdownUserMenuLink')
+    let subMenuProfile = document.getElementById('subMenu')
+    menuProfile?.classList.add('show')
+    subMenuProfile?.classList.add('show')
+  
+  }
+
+  //Funcion para quitar el menu desplegable
+@HostListener('window:click', ['$event'])
+onWindowClick(event: Event) {
+  const dropdownMenu = document.querySelector('.dropdown-menu');
+  const dropdownUserMenuLink = document.querySelector('#dropdownUserMenuLink');
+
+  if (dropdownMenu && dropdownMenu.classList.contains('show') && !dropdownUserMenuLink?.contains(event.target as Node)) {
+    dropdownMenu.classList.remove('show');
+  }
+}
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit   } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {
+   
+   
+  }
+
+
+  changeLineBusTab(tab: string) {
+    let lineBusTab = document.getElementById('lineBusTab');
+    let favoritesTab = document.getElementById('favoritesTab');
+    let linesBusContent = document.getElementById('linesBusContent');
+    let linesFavoriteContent = document.getElementById('linesFavoriteContent');
+    if (tab === 'lineBusTab') {
+      //Eliminar clases
+      favoritesTab?.classList.remove('active');
+      lineBusTab?.classList.remove('active');
+      lineBusTab?.classList.add('active');
+      //Mostrar Contenido
+      linesFavoriteContent?.classList.remove('show', 'active');
+      linesBusContent?.classList.add('show', 'active');
+    }else{
+      lineBusTab?.classList.remove('active')
+    favoritesTab?.classList.add('active')
+
+    //Mostrar Contenido
+    linesBusContent?.classList.remove('show', 'active')
+    linesFavoriteContent?.classList.add('show', 'active')
+    }
+  }
+
+
+
+
+}
